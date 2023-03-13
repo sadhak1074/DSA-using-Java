c7570c110c1640a292fbdf0c2d4ed0a2f7539d61
Sadhak Saini <sadhaksaini18@gmail.com>
2023-03-14 01:56:04 +0530

    solid rectangle using java

---
+++ solid_rectangle.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class solid_rectangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");   
+        int n = sc.nextInt();
+        System.out.println("Enter the number of columns: ");
+        int m = sc.nextInt();
+        for(int i=0; i<=n; i++){
+            for (int j = 0; j <= m; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
